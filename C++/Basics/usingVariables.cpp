be9497cd596d98a3a5dfbaa31aaf00ba6473cdfe
Aryan Das <aryandas079@outlook.com>
2026-08-18 01:38:15 +0530

    made the first program with variable

---
+++ C++/Basics/usingVariables.cpp
@@ -0,0 +1,9 @@
+#include<iostream> 
+using namespace std; 
+int main() { 
+    // taking some values 
+    int x = 10; 
+    int y = x + 10; 
+    cout<<"value of x is: "<<x<<endl; 
+    cout<<"value of y is: "<<y<<endl; 
+}
